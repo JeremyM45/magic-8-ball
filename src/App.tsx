7c973f52662cdbae61fe914a8f4ffbe74f236fab
JeremyM45 <jeremyvs1945@gmail.com>
2023-01-11 14:22:49 -0800

    add base MagicBall

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MagicBall from './components/MagicBall';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1 className="text-3xl font-bold underline text-blue-600">
+        <div className="App w-screen h-screen bg-slate-900">
+            <header className="App-header flex items-center justify-center flex-col">
+                <h1 className="text-3xl font-bold underline text-blue-600 text-center">
                     Magic 8 Ball
                 </h1>
+                <div className='py-20' />
+                <MagicBall />
             </header>
         </div>
     );
---
+++ src/components/MagicBall.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MagicBall = () => {
+    return (
+        <div className='bg-black w-[20rem] h-[20rem] rounded-full flex items-center justify-center'>
+            <div className='bg-white w-[8rem] h-[8rem] rounded-full flex items-center justify-center'>
+                <p className='font-bold text-6xl'>8</p>
+            </div>
+        </div>
+    )
+}
+
+export default MagicBall
