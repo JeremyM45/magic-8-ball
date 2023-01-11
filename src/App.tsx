0a44ce8524d24cb9bbc1b324b867af36431dfe2f
JeremyM45 <jeremyvs1945@gmail.com>
2023-01-11 14:43:56 -0800

    add MagicBallProps

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MagicBall from './components/MagicBall';
 
 function App() {
+    const [questionAsked, setQuestionAsked] = useState(false);
     return (
         <div className="App w-screen h-screen bg-slate-900">
             <header className="App-header flex items-center justify-center flex-col">
                 <h1 className="text-3xl font-bold underline text-blue-600 text-center">
                     Magic 8 Ball
                 </h1>
+                
                 <div className='py-20' />
-                <MagicBall />
+                <MagicBall 
+                    response='Reply hazy, try again'
+                    questionAsked={questionAsked}
+                />
             </header>
         </div>
     );
--- src/components/MagicBall.tsx
+++ src/components/MagicBall.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
-const MagicBall = () => {
+interface MagicBallProps {
+    response: string,
+    questionAsked: boolean
+}
+
+const MagicBall = ({response, questionAsked}: MagicBallProps)=> {
+    
     return (
+        
         <div className='bg-black w-[20rem] h-[20rem] rounded-full flex items-center justify-center'>
-            <div className='bg-white w-[8rem] h-[8rem] rounded-full flex items-center justify-center'>
-                <p className='font-bold text-6xl'>8</p>
-            </div>
+            {!questionAsked ? (
+                    <div className='bg-white w-[8rem] h-[8rem] rounded-full flex items-center justify-center'>
+                        <p className='font-bold text-6xl'>8</p>
+                    </div>
+                ) : (
+                    <div className='w-[8rem] h-[8rem] rounded-full flex items-center justify-center'>
+                        <p>{response}</p>
+                    </div>
+                )
+            }
         </div>
     )
 }
