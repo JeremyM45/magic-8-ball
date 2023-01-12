4e3302f824d78bebd47e466d2a38d543c8142af3
JeremyM45 <jeremyvs1945@gmail.com>
2023-01-11 17:58:28 -0800

    add basic styling to question form

--- src/components/QuestionForm.tsx
+++ src/components/QuestionForm.tsx
@@ -15,9 +15,10 @@ const QuestionForm = ({setQuestionAsked, setResponse, responses}: any) => {
     
     return (
         <div>
-            <form className='flex flex-col gap-y-2' onSubmit={handleSubmit}>
-                <input className='rounded-full' placeholder='Ask Your Question'/>
-                <button type='submit' className='text-blue-500 rounded-full border-blue-500 border p-2'>submit</button>
+            <form className='flex flex-col gap-y-2 items-center' onSubmit={handleSubmit}>
+                <input className='rounded-full px-3 text-2xl w-[20rem]' placeholder='Ask Your Question'/>
+                <div className='py-2' />
+                <button type='submit' className='text-blue-500 rounded-full border-blue-700 border px-12 p-2 font-semibold hover:bg-blue-800 hover:text-slate-200 duration-[400ms]'>Ask</button>
             </form>
         </div>
     )
