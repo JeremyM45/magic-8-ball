2e2afc4b3c9366b56f4d3d3086717525ed160f3c
JeremyM45 <jeremyvs1945@gmail.com>
2023-01-11 18:07:01 -0800

    add quesion display to QuestionForm

--- src/components/QuestionForm.tsx
+++ src/components/QuestionForm.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import { useState } from 'react'
 
 const QuestionForm = ({setQuestionAsked, setResponse, responses}: any) => {
+    const [question, setQuestion] = useState("");
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setQuestionAsked(true);
@@ -14,12 +15,18 @@ const QuestionForm = ({setQuestionAsked, setResponse, responses}: any) => {
     }
     
     return (
-        <div>
+        <div className='flex items-center flex-col'>
             <form className='flex flex-col gap-y-2 items-center' onSubmit={handleSubmit}>
-                <input className='rounded-full px-3 text-2xl w-[20rem]' placeholder='Ask Your Question'/>
+                <input 
+                    className='rounded-full px-3 text-2xl w-[20rem]' 
+                    placeholder='Ask Your Question'
+                    type='text'
+                    onChange={(e) => setQuestion(e.target.value)}
+                />
                 <div className='py-2' />
                 <button type='submit' className='text-blue-500 rounded-full border-blue-700 border px-12 p-2 font-semibold hover:bg-blue-800 hover:text-slate-200 duration-[400ms]'>Ask</button>
             </form>
+            <p className='text-slate-200 font-semibold text-xl text-center max-w-[40rem] overflow-auto'>{question}</p>
         </div>
     )
 }
